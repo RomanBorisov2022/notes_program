5ac4cab85ed75448f99ebb510b2cdebf57891a76
Romario <boriso-rom@yandex.ru>
2023-06-07 16:37:56 +0300

    last commit

--- main.py
+++ main.py
@@ -12,10 +12,10 @@ def main():
     try:
         notes = load_notes(NOTES_FILENAME)
     except:
-        notes  = []
-    
+        notes = []
+
     while True:
-        print("="*30)
+        print("=" * 30)
         print("Выберите действие:")
         print("1. Просмотреть все заметки")
         print("2. Просмотреть заметки по дате")
@@ -24,14 +24,14 @@ def main():
         print("5. Удалить заметку")
         print("6. Выйти")
         choice = input("Введите номер действия: ")
-        print("="*30)
-    
+        print("=" * 30)
+
         if choice == "1":
             if len(notes) == 0:
                 print("Нет ни одной заметки")
             else:
                 print_notes(notes)
-    
+
         elif choice == "2":
             date_str = input("Введите дату в формате YYYY-MM-DD: ")
             selected_notes = get_notes_by_date(notes, date_str)
@@ -39,22 +39,22 @@ def main():
                 print("Нет заметок по указанной дате")
             else:
                 print_notes(selected_notes)
-    
+
         elif choice == "3":
             add_note(notes)
-    
+
         elif choice == "4":
             id = int(input("Введите ID заметки для редактирования: "))
             edit_note_by_id(notes, id)
-    
+
         elif choice == "5":
             id = int(input("Введите ID заметки для удаления: "))
             delete_note_by_id(notes, id)
-    
+
         elif choice == "6":
             save_notes(notes, NOTES_FILENAME)
             print("До свидания!")
             break
-    
+
         else:
             print("Некорректный ввод. Попробуйте еще раз.")
